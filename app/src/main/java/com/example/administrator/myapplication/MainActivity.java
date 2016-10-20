d3e55b0cfb994fdf3ad3f7754412c94d14b488ef
tie <493820064@qq.com>
2016-10-20 18:44:53 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.administrator.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.andexert.calendarlistview.library.DatePickerController;
+import com.andexert.calendarlistview.library.DayPickerView;
+import com.andexert.calendarlistview.library.SimpleMonthAdapter;
+
+public class MainActivity extends AppCompatActivity implements DatePickerController {
+
+    private DayPickerView mCalendar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mCalendar = (DayPickerView) findViewById(R.id.pickerView);
+        mCalendar.setController(this);
+    }
+
+    @Override
+    public int getMaxYear() {
+        return 2016;
+    }
+
+    @Override
+    public void onDayOfMonthSelected(int year, int month, int day) {
+
+    }
+
+    @Override
+    public void onDateRangeSelected(SimpleMonthAdapter.SelectedDays<SimpleMonthAdapter.CalendarDay> selectedDays) {
+
+    }
+}
